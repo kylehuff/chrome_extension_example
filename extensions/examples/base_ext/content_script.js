af8baf893a466ec0ce4006ef45fff982534bd910
Kyle L. Huff <github.com@rfgate.com>
2010-12-23 11:05:53 -0800

    The initial base extension

---
+++ extensions/examples/base_ext/content_script.js
@@ -0,0 +1,20 @@
+
+var myContentScript = {
+
+    onLoad: function() {
+        console.log("Loaded....");
+        // Create a port to the background page for long-lived connections
+        var port = chrome.extension.connect({"name": "your content script"});
+        port.postMessage({"msg": "I have a message for you..."});
+        port.onMessage.addListener(function(msg) {
+            myContentScript.onPortMessage(port, msg);
+        });
+    },
+
+    onPortMessage: function(port, msg) {
+        console.log("we have received a message from ", port, " here is the message: ", msg);
+    },
+
+};
+
+myContentScript.onLoad();
